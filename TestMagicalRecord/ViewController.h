28418556b06aed5e492d6add69487ebd865994ae
村上智紀 <fankami@murakami.local>
2014-10-28 17:46:32 +0900

    Initial Commit

---
+++ TestMagicalRecord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestMagicalRecord
+//
+//  Created by 村上智紀 on 2014/10/28.
+//  Copyright (c) 2014年 pad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
